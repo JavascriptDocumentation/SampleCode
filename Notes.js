3cf98fd1c15566fb25325331a7ef875c67b1a5be
Vuk Dukic <vuk.dukic@clara.com>
2016-09-30 14:11:31 -0700

    Create Notes.js

---
+++ Notes.js
@@ -0,0 +1,28 @@
+var age = prompt("What is your age?")
+
+console.log(age);
+
+// coersion = conversion in javascript
+// explicit and implicit
+
+a = String(a);
+
+b = Number(a);
+
+// In javascript variables do not have type
+
+// implicit coersion
+
+// explicit coersion
+
+a = a + "";
+
+const a = 42; // a can never be changed
+
+var a = 42; // a variables
+
+var a = 42;
+
+let b = 2;
+
+const a = 42;
